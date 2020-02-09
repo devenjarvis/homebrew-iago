a1dfcdca88c8e35c5f98f6b94be07ef17fb306ad
Deven Jarvis <devenjarvis@gmail.com>
2020-02-09 12:51:43 -0500

    chore: version bump

--- formula/iago.rb
+++ formula/iago.rb
@@ -4,7 +4,7 @@
 class Iago < Formula
   desc "Turn any image into a party parrot"
   homepage "https://github.com/devenjarvis/iago"
-  url "https://github.com/devenjarvis/iago/archive/v0.0.1.tar.gz"
+  url "https://github.com/devenjarvis/iago/archive/v0.0.3.tar.gz"
   sha256 "a0ce6b3a9d1166cd8595393518126ce6f55c499dc349b05e1e58e5d3415e915b"
 
   # depends_on "cmake" => :build
