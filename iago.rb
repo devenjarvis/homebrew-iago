3ac3ca0d937d7f99bc44bf902073e41823a0bb14
Deven Jarvis <devenjarvis@devens-mbp.lan>
2020-02-09 12:31:03 -0500

    fix: add a homepage

--- iago.rb
+++ iago.rb
@@ -3,7 +3,7 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Iago < Formula
   desc "Turn any image into a party parrot"
-  homepage ""
+  homepage "https://github.com/devenjarvis/iago"
   url "https://github.com/devenjarvis/iago/archive/v0.0.1.tar.gz"
   sha256 "a0ce6b3a9d1166cd8595393518126ce6f55c499dc349b05e1e58e5d3415e915b"
 
